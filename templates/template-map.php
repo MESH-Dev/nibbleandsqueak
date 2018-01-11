91c6f34a0cfcd0fb89c7bc3677c7f0e25e95c499
Shaun Dover <shaundover@Megans-MacBook-Pro-2.local>
2018-01-11 16:33:35 -0500

    QA round 1

--- templates/template-map.php
+++ templates/template-map.php
@@ -6,7 +6,7 @@ get_header(); ?>
 <main class="map-page" id="content">
 		<div class="row">
 			<div class="locations">
-				<div class="">
+				<!-- <div class=""> -->
 				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 					<h2><?php the_title(); ?></h2>
@@ -29,7 +29,7 @@ get_header(); ?>
 						}else{ 
 						get_template_part('partials/city-map');
 					}?>
-				 </div>        
+				 <!-- </div>  -->       
 			</div>
 		</div>
 	<div id="map"></div>
