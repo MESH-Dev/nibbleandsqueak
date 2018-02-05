c8fdc7012b4fcb41ed59cdb78af7d9ebfd5ebac1
Shaun <shaun@meshfresh.com>
2018-02-05 14:27:45 -0500

    Geolocation updates 2

--- partials/bubble.php
+++ partials/bubble.php
@@ -7,9 +7,19 @@
 	$l_id = get_id_by_slug($city_slug);
 	
 	$cta_text = get_field('cta_text', $l_id);
-	$cta_link = get_field('cta_link', $l_id);
+	//$cta_link = get_field('cta_link', $l_id);
 	
 	$cb_external = get_field('cb_external', $l_id);
+
+	$home_id = get_option('page_on_front');
+
+	if(is_frontpage()){
+		$cta_link = get_field('cta_link', $l_id);
+		$cta_text = get_field('cta_text', $l_id);
+	}else{
+		$cta_link = get_field('cta_link', $home_id);
+		$cta_text = get_field('cta_text', $home_id);
+	}
 	
 	if($cb_external == true){
 		$bubble_target='target="_blank"';
